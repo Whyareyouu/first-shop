acf211b134ab8e82f65331dcc9136bbba56f3ab1
whyareyou <bionixxxd5@gmail.com>
2023-01-13 21:42:22 +0300

    added layout

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,9 +1,19 @@
 import { FunctionComponent } from 'react';
 import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
+import { Header } from './Header/Header';
+import { Navbar } from './Navbar/Navbar';
+import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
-	return <div className={styles.wrapper}>{children}</div>;
+	return (
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Navbar className={styles.navbar} />
+			<main className={styles.main}>{children}</main>
+			<Footer className={styles.footer} />
+		</div>
+	);
 };
 
 export const withLayout = <T extends Record<string, unknown>>(
---
+++ layouts/Navbar/Navbar.tsx
@@ -0,0 +1,3 @@
+export const Navbar = ({ ...props }) => {
+	return <nav {...props}>Навигация</nav>;
+};
